d66d528359fa8ef95660e74429496cba03c9d656
Josh Justice <jjustice@bignerdranch.com>
2018-05-16 18:42:39 -0400

    Pod update

--- ios/ReactNativeExistingiOS/ViewController.swift
+++ ios/ReactNativeExistingiOS/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import React
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showRNThing(_ sender: Any) {
+        let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios")
+    }
+    
 }
 
