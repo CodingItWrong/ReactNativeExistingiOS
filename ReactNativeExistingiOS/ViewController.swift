3d61dc2d13a59972dc48862f9a930c59fe853a52
Josh Justice <jjustice@bignerdranch.com>
2018-05-16 05:02:32 -0400

    Create project

---
+++ ReactNativeExistingiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReactNativeExistingiOS
+//
+//  Created by Josh Justice on 5/16/18.
+//  Copyright © 2018 Josh Justice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
